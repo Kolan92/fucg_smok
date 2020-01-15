b1cf4f1d587e80b08caf27922efa8ed910bbd642
Paweł Kolanowski <pawel.krzysztof.kolanowski@gmail.com>
2020-01-15 19:50:15 +0100

    Add percentage option

--- airly_client/airly_client.py
+++ airly_client/airly_client.py
@@ -3,24 +3,23 @@ import requests
 class AirlyClient:
 
     _url = 'https://airapi.airly.eu/v2/measurements/nearest?indexType=AIRLY_CAQI&lat=50.062006&lng=19.940984&maxDistanceKM=3'
+    _percent = 0
     _token = ''
 
-    def f(self) -> str:
-        return 'hello world'
-#map(lambda x: x + x, numbers) 
-    def get_forecast(self):
+    def get_hours_above_limit_in_next_day(self) -> int:
         session = requests.Session()
         response = session.get(self._url, headers={'Accept': 'application/json', 'apikey': self._token })
         forecast = response.json()['forecast']
-        standards = [s for s in forecast 
-            if any(p > 100 for p in map(lambda x: x['percent'], s['standards']))]
+        standards = [f for f in forecast 
+            if any(p > self._percent for p in map(lambda x: x['percent'], f['standards']))]
 
         print(standards)
         print('\n\n\n')
-        print(len(standards))
+        return len(standards)
         
 
 
 
-    def __init__(self, token: str):
+    def __init__(self, token: str, percent: int):
         self._token = token
+        self._percent = percent
--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("token", help="airly token used to authenticate Airly API calls")
+    parser.add_argument("--percent", "-p", type=int, help="percent threshold above which one raise alarm")
     args = parser.parse_args()
     print(args.token)
-    airly = AirlyClient(args.token)
-    airly.get_forecast()
+    percent = args.percent if args.percent else 200
+    airly = AirlyClient(args.token, percent)
+    hours = airly.get_hours_above_limit_in_next_day()
+    print("In the next 24h there will be {} hours with air pollution above {}% WHO limit".format(hours, percent))
     
 if __name__ == '__main__':
     main()
